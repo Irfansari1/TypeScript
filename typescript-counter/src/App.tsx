a150fcb97b7fcc7ce29c5b6537e14af16548dd5c
Irfansari1 <irfansari42@gmail.com>
2022-03-23 00:30:10 +0100

    TS Counter

---
+++ typescript-counter/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { Counter } from './components/state/Counter';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <Counter/>
+    </div>
+    
+    </>
+  
+  )
+}
+
+export default App;
