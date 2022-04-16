8b4d018a9822dd60532cf9e8ce3e5921a0d4fe15
Irfansari1 <irfansari42@gmail.com>
2022-04-16 14:21:12 +0200

    classcomponents added

---
+++ classcomponent/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Counter } from './components/class/counter';
+
+function App() {
+  return ( 
+  <div className="App">
+    <Counter message='The count value is'/>
+  </div>
+  )
+}
+
+export default App
