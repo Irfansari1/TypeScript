a52e143a96b91fecadffa97123c2d5fc442359cd
Irfansari1 <irfansari42@gmail.com>
2022-04-23 17:40:58 +0200

    templateliterals

---
+++ templateliterals/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Toast } from './templateliterals/Toast';
+
+function App() {
+  return (
+    <div className="App">
+      <Toast position ='center-bottom'/>
+    </div>
+  );
+}
+
+export default App;
