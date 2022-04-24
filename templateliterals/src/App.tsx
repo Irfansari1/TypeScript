6bbef9419fd91a09373f176eac1d9a654693116f
Irfansari1 <irfansari42@gmail.com>
2022-04-24 23:44:51 +0200

    Wrapping HTML Elements

--- templateliterals/src/App.tsx
+++ templateliterals/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
+import { CustomButton } from './html/Button';
 import { Toast } from './templateliterals/Toast';
 
 function App() {
   return (
     <div className="App">
       <Toast position ='center-bottom'/>
+      <CustomButton variant='primary' onClick={() => console.log('Clicked')}>
+        Primary Button
+      </CustomButton>
     </div>
   );
 }
