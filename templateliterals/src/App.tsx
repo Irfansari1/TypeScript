e2ec4b1a05e430253f570199139ae06c24465dac
Irfansari1 <irfansari42@gmail.com>
2022-04-26 14:32:29 +0200

    polymorphic

--- templateliterals/src/App.tsx
+++ templateliterals/src/App.tsx
@@ -1,14 +1,24 @@
 import './App.css';
 import { CustomButton } from './html/Button';
+import { Text } from './polymorphic/Text';
 import { Toast } from './templateliterals/Toast';
 
 function App() {
   return (
     <div className="App">
-      <Toast position ='center-bottom'/>
-      <CustomButton variant='primary' onClick={() => console.log('Clicked')}>
+      <Toast position="center-bottom" />
+      <CustomButton variant="primary" onClick={() => console.log("Clicked")}>
         Primary Button
       </CustomButton>
+      <Text as="h1" size="lg">
+        Heading
+      </Text>
+      <Text as="p" size="md">
+        Paragraph
+      </Text>
+      <Text as="label" htmlFor ='some' size="sm" color="secondary">
+        Label
+      </Text>
     </div>
   );
 }
